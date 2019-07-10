72d62d8ecde6ec5f48f67473f4615c1efe7cf049
Carlo Gilmar <carlogilmar12@gmail.com>
2019-07-10 13:20:46 -0500

    #2 Create job domain
    #2 Adding job model
    
    #2 Adding migration

---
+++ lib/remote_jobs/models/job.ex
@@ -0,0 +1,29 @@
+defmodule RemoteJobs.Job do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+	@primary_key {:id, :id, autogenerate: true}
+  schema "jobs" do
+    field :position, :string
+    field :company_name, :string
+    field :location_restricted, :string
+    field :primary_tag, :string
+    field :extra_tags, :string
+    field :salary, :string
+    field :description, :string
+    field :responsabilities, :string
+    field :requirements, :string
+    field :apply_description, :string
+    field :url, :string
+    field :email, :string
+    field :logo, :string
+    timestamps()
+  end
+
+  @doc false
+  def changeset(job, attrs) do
+    job
+    |> cast(attrs, [])
+    |> validate_required([])
+  end
+end
---
+++ priv/repo/migrations/20190710175729_add_job.exs
@@ -0,0 +1,23 @@
+defmodule RemoteJobs.Repo.Migrations.AddJob do
+  use Ecto.Migration
+
+  def change do
+		create table(:jobs, primary_key: false) do
+      add :id, :serial, primary_key: true
+			add :position, :string
+			add :company_name, :string
+			add :location_restricted, :string
+			add :primary_tag, :string
+			add :extra_tags, :string
+			add :salary, :string
+			add :description, :string
+			add :responsabilities, :string
+			add :requirements, :string
+			add :apply_description, :string
+			add :url, :string
+			add :email, :string
+			add :logo, :string
+			timestamps()
+		end
+  end
+end
