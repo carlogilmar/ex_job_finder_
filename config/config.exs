df847184ad78e644638d47bb9e404589ba921196
Ivan Alvarez Frias <ivan.alvarez.frias@gmail.com>
2019-08-03 23:28:57 -0500

    #10 debug level to test slack bot

--- config/config.exs
+++ config/config.exs
@@ -23,7 +23,7 @@ config :logger,
 
 config :logger, :remote_jobs,
   path: "remote_jobs.log",
-  level: :info
+  level: :debug
 
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
