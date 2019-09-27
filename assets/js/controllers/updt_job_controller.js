ba01a10e1db05954d6cf2d7b1785d31a03c79bee
Carlo Gilmar <carlogilmar12@gmail.com>
2019-09-27 17:49:45 -0500

    #75 Adding notifications in view

--- assets/js/controllers/updt_job_controller.js
+++ assets/js/controllers/updt_job_controller.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import socket from "./../socket"
+import Notifications from 'vue-notification'
+Vue.use(Notifications)
 
 export const app = new Vue({
 	el:"#app",
   data: {
+    job: {
+      position: null
+    }
   },
 	created: function() {
     let job = document.getElementById("job").value
@@ -11,11 +16,36 @@ export const app = new Vue({
 		this.channel.join()
 			.receive("ok", resp => {
 				console.log("Joined successfully", resp);
+        this.job = resp;
+        console.log(this.job);
 			})
 			.receive("error", resp => {
 				console.log("Unable to join", resp);
 			});
 	},
   methods: {
+  /*
+   * Notifications
+   * */
+    notify: function(type, title, context){
+      this.$notify({
+        group: 'foo',
+        type: type,
+        title: title,
+        text: context,
+        duration: 1000,
+        speed: 1000
+      })
+    },
+    upt_position: function(value, attribute){
+      this.update_job(value, attribute, this.job.id);
+    },
+    update_job: function(value, attribute,  job_id){
+      this.channel.push("updt_job:update", {job: job_id, attribute: attribute, value: value})
+        .receive('ok', (res) => {
+          this.notify('info', 'Vacante Actualizada', '');
+        })
+        .receive("error", resp => { this.notify('error', 'No se pudo actualizar', ''); });
+    }
   }
 });
--- lib/remote_jobs_web/channels/updt_job_channel.ex
+++ lib/remote_jobs_web/channels/updt_job_channel.ex
@@ -4,10 +4,17 @@ defmodule RemoteJobsWeb.UpdtJobChannel do
   """
 
   use Phoenix.Channel
-  alias RemoteJobs.JobManager
 
   def join("updt_job:join", msg, socket) do
-    {:ok, [], socket}
+    job = %{id: 1, position: "dev", company_name: "md"}
+    {:ok, job, socket}
+  end
+
+  def handle_in("updt_job:update", %{"attribute" => attribute, "job" => job_id, "value" => value}, socket) do
+    IO.puts attribute
+    IO.puts job_id
+    IO.puts value
+    {:reply, {:ok, %{status: "200"}}, socket}
   end
 
 end
