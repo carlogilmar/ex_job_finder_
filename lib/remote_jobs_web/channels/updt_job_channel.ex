4ef641d1b0ab4569388014b4519d2079a97651d2
Carlo Gilmar <carlogilmar12@gmail.com>
2019-09-27 18:10:30 -0500

    #75 Getting job from store

--- lib/remote_jobs_web/channels/updt_job_channel.ex
+++ lib/remote_jobs_web/channels/updt_job_channel.ex
@@ -2,11 +2,16 @@ defmodule RemoteJobsWeb.UpdtJobChannel do
   @moduledoc """
     A module in charge of the job channel.
   """
-
   use Phoenix.Channel
+  alias RemoteJobs.JobOperator
 
-  def join("updt_job:join", msg, socket) do
-    job = %{id: 1, position: "dev", company_name: "md"}
+  def join("updt_job:join", %{"job" => job_id}, socket) do
+    job =
+      job_id
+      |> String.to_integer()
+      |> JobOperator.find()
+      |> UpdtJobUtil.normalize_job()
+
     {:ok, job, socket}
   end
 
@@ -15,6 +20,20 @@ defmodule RemoteJobsWeb.UpdtJobChannel do
     IO.puts job_id
     IO.puts value
     {:reply, {:ok, %{status: "200"}}, socket}
+  end
+
+end
+
+defmodule UpdtJobUtil do
+
+  def normalize_job(job) do
+    job
+      |> Map.put(:__meta__, "")
+      |> Map.put(:expire_date, Date.to_string(job.expire_date))
+      |> Map.put(:inserted_at, "")
+      |> Map.put(:track, "")
+      |> Map.put(:updated_at, "")
+      |> Map.from_struct()
   end
 
 end
