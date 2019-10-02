8d4fd123f9b6dc757b1fe99e06a2be5eeb80af4c
Carlo Gilmar <carlogilmar12@gmail.com>
2019-10-02 14:03:52 -0500

    #76 Adding error session msg in view

--- lib/remote_jobs_web/controllers/login_controller.ex
+++ lib/remote_jobs_web/controllers/login_controller.ex
@@ -5,12 +5,16 @@ defmodule RemoteJobsWeb.LoginController do
 		form_params = Map.has_key?(params, "_csrf_token")
 		v = fn
 			false ->
-				render(conn, "index.html")
+				render(conn, "index.html", error: "")
 			true ->
 				user = %{id: 1, username: "carlogilmar", password: "1234567890"}
 				login_reply({:ok, user}, conn)
 		end
 		v.(form_params)
+	end
+
+	defp error_in_login(conn) do
+		conn |> render("index.html", error: "Error al iniciar sesión")
 	end
 
   defp login_reply({:ok, user}, conn) do
