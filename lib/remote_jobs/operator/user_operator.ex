4f3d1ce89ae512f58d4961df6d15b9cd8a47bcfb
Carlo Gilmar <carlogilmar12@gmail.com>
2019-10-02 11:10:36 -0500

    #76 Adding user operator

---
+++ lib/remote_jobs/operator/user_operator.ex
@@ -0,0 +1,16 @@
+defmodule RemoteJobs.UserOperator do
+  @moduledoc """
+    User operator definition
+  """
+  alias RemoteJobs.User
+
+  def create_user(username, role, password) do
+    hash = hash_password(password)
+    %User{}
+    |> User.changeset(%{username: username, password: hash, role: role})
+    |> Repo.insert()
+  end
+
+  defp hash_password(password), do: Argon2.hash_pwd_salt(password)
+
+end
