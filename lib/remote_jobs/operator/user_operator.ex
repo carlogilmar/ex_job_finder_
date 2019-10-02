5ac54b6300c7f6aed4f53fa56f7b6d436a7948de
Carlo Gilmar <carlogilmar12@gmail.com>
2019-10-02 11:30:16 -0500

    #76 Getting user by username

--- lib/remote_jobs/operator/user_operator.ex
+++ lib/remote_jobs/operator/user_operator.ex
@@ -2,6 +2,7 @@ defmodule RemoteJobs.UserOperator do
   @moduledoc """
     User operator definition
   """
+  import Ecto.Query, only: [from: 2]
   alias RemoteJobs.Repo
   alias RemoteJobs.User
 
@@ -10,6 +11,13 @@ defmodule RemoteJobs.UserOperator do
     %User{}
     |> User.changeset(%{username: username, password: hash, role: role})
     |> Repo.insert()
+  end
+
+  def get_by_username(username) do
+    query =
+      from(user in User, where: user.username == ^username)
+
+    query |> Repo.one()
   end
 
   defp hash_password(password), do: Argon2.hash_pwd_salt(password)
