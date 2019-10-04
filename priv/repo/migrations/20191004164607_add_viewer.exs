f97e69640c14891975826778ae22d0dc8e049bb8
Carlo Gilmar <carlogilmar12@gmail.com>
2019-10-04 12:08:30 -0500

    #76 Adding viewer migration and model

---
+++ priv/repo/migrations/20191004164607_add_viewer.exs
@@ -0,0 +1,19 @@
+defmodule RemoteJobs.Repo.Migrations.AddViewer do
+  use Ecto.Migration
+
+  def change do
+    create table(:viewers, primary_key: false) do
+      add :id, :serial, primary_key: true
+      add :city, :string
+			add :continent, :string
+			add :country, :string
+			add :region, :string
+			add :time_zone, :string
+			add :latitude, :float
+			add :longitude, :float
+			timestamps()
+    end
+  end
+end
+
+
