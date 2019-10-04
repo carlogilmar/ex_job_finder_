e4750e15f2c163c8e944e77e27bef34b9bc4ee87
Carlo Gilmar <carlogilmar12@gmail.com>
2019-10-03 23:11:26 -0500

    #76 Connecting valid user with guardian session

--- lib/remote_jobs_web/controllers/login_controller.ex
+++ lib/remote_jobs_web/controllers/login_controller.ex
@@ -1,25 +1,29 @@
 defmodule RemoteJobsWeb.LoginController do
   use RemoteJobsWeb, :controller
+	alias RemoteJobs.Session
 
   def index(conn, params) do
 		form_params = Map.has_key?(params, "_csrf_token")
-		v = fn
-			false ->
-				render(conn, "index.html", error: "")
-			true ->
-				user = %{id: 1, username: "carlogilmar", password: "1234567890"}
-				login_reply({:ok, user}, conn)
-		end
-		v.(form_params)
+		validate_params.({form_params, conn, params})
 	end
 
-	defp error_in_login(conn) do
-		conn |> render("index.html", error: "Error al iniciar sesión")
+  defp validate_params do
+		fn
+			{false, conn, _params} ->
+				render(conn, "index.html", error: "")
+			{true, conn, params} ->
+				session = Session.auth_user(params["username"], params["password"])
+				login_reply(session, conn)
+		end
+	end
+
+	defp login_reply({:error, error}, conn) do
+		conn |> render("index.html", error: error)
 	end
 
   defp login_reply({:ok, user}, conn) do
-    user = RemoteJobs.Guardian.encode_and_sign(user)
-    RemoteJobs.Guardian.Plug.sign_in(conn, user) |> redirect(to: "/auth")
+    session = RemoteJobs.Guardian.encode_and_sign(user)
+    RemoteJobs.Guardian.Plug.sign_in(conn, session) |> redirect(to: "/auth")
   end
 
   def home(conn, params) do
