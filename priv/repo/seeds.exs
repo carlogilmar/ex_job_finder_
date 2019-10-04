858812d1f5cf34491b29ee2f2ffaeb94ce7c6fd6
Carlo Gilmar <carlogilmar12@gmail.com>
2019-10-03 23:17:44 -0500

    #76 Adding seed for add admin user

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,4 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+RemoteJobs.UserOperator.create_user("codigoambar", "ADMIN", "c0d1g04mb4r")
