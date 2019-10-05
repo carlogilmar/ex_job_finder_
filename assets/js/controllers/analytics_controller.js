eeac075228ef704655a2b5c56baad4009e74ce17
Carlo Gilmar <carlogilmar12@gmail.com>
2019-10-04 19:28:21 -0500

    #76 Getting analytics

--- assets/js/controllers/analytics_controller.js
+++ assets/js/controllers/analytics_controller.js
@@ -3,96 +3,11 @@ import socket from "./../socket"
 import VueApexCharts from 'vue-apexcharts'
 Vue.component('apexchart', VueApexCharts)
 
-function generateData(count, yrange) {
-  console.log("Generating data");
-      var i = 0;
-      var series = [];
-      while (i < count) {
-        var x = 'w' + (i + 1).toString();
-        var y = Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
-
-        series.push({
-          x: x,
-          y: y
-        });
-        i++;
-      }
-      return series;
-    }
-
 export const app = new Vue({
 	el:"#app",
   data: {
-		series: [{
-			name: 'Metric1',
-			data: [
-        {x: "Domingo", y:90},
-        {x: "Lunes", y:90},
-        {x: "Martes", y:90},
-        {x: "Miércoles", y:90},
-        {x: "Jueves", y:90},
-        {x: "Viernes", y: 150},
-        {x: "Sábado", y: 100}
-      ]
-
-		},
-			{
-				name: 'Metric2',
-				data: generateData(18, {
-					min: 0,
-					max: 90
-				})
-			},
-			{
-				name: 'Metric3',
-				data: generateData(18, {
-					min: 0,
-					max: 90
-				})
-			},
-			{
-				name: 'Metric4',
-				data: generateData(18, {
-					min: 0,
-					max: 90
-				})
-			},
-			{
-				name: 'Metric5',
-				data: generateData(18, {
-					min: 0,
-					max: 90
-				})
-			},
-			{
-				name: 'Metric6',
-				data: generateData(18, {
-					min: 0,
-					max: 90
-				})
-			},
-			{
-				name: 'Metric7',
-				data: generateData(18, {
-					min: 0,
-					max: 90
-				})
-			},
-			{
-				name: 'Metric8',
-				data: generateData(18, {
-					min: 0,
-					max: 90
-				})
-			},
-			{
-				name: 'Metric9',
-				data: generateData(18, {
-					min: 0,
-					max: 90
-				})
-			}
-		],
+		loader: true,
+		series: [],
 		chartOptions: {
 			dataLabels: {
 				enabled: false
@@ -101,19 +16,23 @@ export const app = new Vue({
 			title: {
 				text: ''
 			}
-		}
+		},
+    counter: 0,
+    info: []
   },
 	created: function() {
 		this.channel = socket.channel("analytics:join", {});
 		this.channel.join()
 			.receive("ok", resp => {
-				console.log("Joined successfully", resp);
-        this.series = resp;
+				console.log("Joined successfully");
+        this.series = resp.analytics;
+        this.counter = resp.counter;
+        this.info = resp.info;
+				this.loader = false;
 			})
 			.receive("error", resp => {
 				console.log("Unable to join", resp);
 			});
-    console.log(this.series);
 	},
   methods: {
   }
