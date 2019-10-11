9e719a04ca803e81357daa0ce76763d542c3f01f
Carlo Gilmar <carlogilmar12@gmail.com>
2019-10-11 12:39:56 -0500

    #84 Add profile schema

---
+++ lib/remote_jobs/models/profile.ex
@@ -0,0 +1,25 @@
+defmodule RemoteJobs.Profile do
+  @moduledoc """
+    Profile model definition
+  """
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :id, autogenerate: true}
+  schema "jobs" do
+    field :name, :string
+    field :description, :string, default: "Sin descripción"
+    field :experience, :string, default: "Sin experiencia"
+    field :email, :string, "Sin email"
+    field :phone, :string, "Sin teléfono"
+    timestamps()
+    has_many :skill, Skill, on_delete: :delete_all
+  end
+
+  @doc false
+  def changeset(job, attrs) do
+    job
+    |> cast(attrs, [:name, :description, :experience, :email, :phone]
+    |> validate_required([])
+  end
+end
