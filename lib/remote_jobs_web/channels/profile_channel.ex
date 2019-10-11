b698607265ababa3e610e6c13f7b8fdb10c0dbed
Carlo Gilmar <carlogilmar12@gmail.com>
2019-10-11 18:05:57 -0500

    #84 Implement channel socket

--- lib/remote_jobs_web/channels/profile_channel.ex
+++ lib/remote_jobs_web/channels/profile_channel.ex
@@ -3,13 +3,36 @@ defmodule RemoteJobsWeb.ProfileChannel do
     A module in charge of the profile channel.
   """
   use Phoenix.Channel
-  alias RemoteJobs.ParserUtil
+  alias RemoteJobs.DateUtil
+  alias RemoteJobs.ProfileOperator
 
-  def join("profile:join", %{"profile" => profile}, socket) do
-    IO.puts "***************"
-    IO.inspect profile
-    IO.puts "***************"
-    {:ok, %{}, socket}
+  def join("profile:join", %{"profile" => profile_id}, socket) do
+    profile =
+      profile_id
+      |> String.to_integer()
+      |> ProfileOperator.get_by_id()
+      |> normalize_for_show_in_view()
+    {:ok, %{profile: profile}, socket}
+  end
+
+  def handle_in("profile:update", %{"attribute" => attribute, "profile" => profile_id, "value" => value}, socket) do
+    attrs = Map.new [{String.to_atom(attribute), value}]
+    _ = ProfileOperator.update(profile_id, attrs)
+    {:reply, {:ok, %{status: "200"}}, socket}
+  end
+
+  def normalize_for_show_in_view(profile) do
+    date_parsed = NaiveDateTime.to_string(profile.inserted_at)
+    inserted_at = DateUtil.convert_to_spanish_date_and_hour(date_parsed)
+    %{
+      description: profile.description,
+      email: profile.email,
+      experience: profile.experience,
+      id: profile.id,
+      inserted_at: inserted_at,
+      name: profile.name,
+      phone: profile.phone,
+    }
   end
 
 end
