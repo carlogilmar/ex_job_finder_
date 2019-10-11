790ef01d3b369e1a6340961a41055c9635b13853
Carlo Gilmar <carlogilmar12@gmail.com>
2019-10-11 12:39:32 -0500

    #84 Add migration for new tables

---
+++ priv/repo/migrations/20191011172705_add_profiles.exs
@@ -0,0 +1,22 @@
+defmodule RemoteJobs.Repo.Migrations.AddProfiles do
+  use Ecto.Migration
+
+  def change do
+    create table(:profiles, primary_key: false) do
+      add :id, :serial, primary_key: true
+      add :name, :string
+      add :description, :text,
+      add :experience, :text
+      add :email, :string
+      add :phone, :text
+      timestamps()
+    end
+
+    create table(:skills, primary_key: false) do
+      add :id, :serial, primary_key: true
+      add :description, :text,
+      timestamps()
+      add :profile_id, references(:profiles)
+    end
+  end
+end
