4f61d8f95a1c631d031472414eb93102df23cd57
Carlo Gilmar <carlogilmar12@gmail.com>
2019-10-11 20:48:54 -0500

    #84 getting tracking in channel

--- lib/remote_jobs_web/channels/profile_channel.ex
+++ lib/remote_jobs_web/channels/profile_channel.ex
@@ -7,8 +7,8 @@ defmodule RemoteJobsWeb.ProfileChannel do
   alias RemoteJobs.ProfileOperator
 
   def join("profile:join", %{"profile" => profile_id}, socket) do
-    {profile, skills} = get_profile(profile_id)
-    {:ok, %{profile: profile, skills: skills}, socket}
+    {profile, skills, tracks} = get_profile(profile_id)
+    {:ok, %{profile: profile, skills: skills, tracks: tracks}, socket}
   end
 
   def handle_in("profile:update", %{"attribute" => attribute, "profile" => profile_id, "value" => value}, socket) do
@@ -19,14 +19,14 @@ defmodule RemoteJobsWeb.ProfileChannel do
 
   def handle_in("profile:add_skill", %{"skill" => description, "profile" => profile_id}, socket) do
 		_ = ProfileOperator.add_skill(profile_id, description)
-    {profile, skills} = get_profile(profile_id)
-    {:reply, {:ok, %{status: "200", profile: profile, skills: skills}}, socket}
+    {profile, skills, tracks} = get_profile(profile_id)
+    {:reply, {:ok, %{status: "200", profile: profile, skills: skills, tracks: tracks}}, socket}
   end
 
   def handle_in("profile:delete_skill", %{"skill" => skill_id, "profile" => profile_id}, socket) do
 		_ = ProfileOperator.delete_skill(skill_id)
-    {profile, skills} = get_profile(profile_id)
-    {:reply, {:ok, %{status: "200", profile: profile, skills: skills}}, socket}
+    {profile, skills, tracks} = get_profile(profile_id)
+    {:reply, {:ok, %{status: "200", profile: profile, skills: skills, tracks: tracks}}, socket}
   end
 
 	def get_profile(profile_id) do
@@ -38,7 +38,8 @@ defmodule RemoteJobsWeb.ProfileChannel do
   def normalize_for_show_in_view(profile) do
     date_parsed = NaiveDateTime.to_string(profile.inserted_at)
     inserted_at = DateUtil.convert_to_spanish_date_and_hour(date_parsed)
-		skills = normalize_skills(profile.skill)
+		skills = normalize_skills_and_tracks(profile.skill)
+		tracks = normalize_skills_and_tracks(profile.candidate_track)
 		profile =
     %{
       description: profile.description,
@@ -49,17 +50,22 @@ defmodule RemoteJobsWeb.ProfileChannel do
       name: profile.name,
       phone: profile.phone,
     }
-   {profile, skills}
+   {profile, skills, tracks}
   end
 
-	def normalize_skills([]), do: []
-  def normalize_skills(skills) do
-		for skill <- skills do
-			%{
-				id: skill.id,
-				description: skill.description
-				}
-		end
+	def normalize_skills_and_tracks([]), do: []
+  def normalize_skills_and_tracks(skills) do
+		normalized_skills =
+		  for skill <- skills do
+		  	date_parsed = NaiveDateTime.to_string(skill.inserted_at)
+		  	inserted_at = DateUtil.convert_to_spanish_date_and_hour(date_parsed)
+		  	%{
+		  		id: skill.id,
+		  		description: skill.description,
+		  		inserted_at: inserted_at
+		  		}
+		  end
+		Enum.reverse(normalized_skills)
 	end
 
 end
