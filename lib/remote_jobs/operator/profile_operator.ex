4cb69d1aade05cca400e11e8c860f111bce6f1aa
Carlo Gilmar <carlogilmar12@gmail.com>
2019-10-11 19:05:03 -0500

    #84 Adding functions for manage skills

--- lib/remote_jobs/operator/profile_operator.ex
+++ lib/remote_jobs/operator/profile_operator.ex
@@ -2,7 +2,7 @@ defmodule RemoteJobs.ProfileOperator do
   @moduledoc """
   A module in charge of profile managment.
   """
-  import Ecto.Query, only: [from: 2]
+  alias RemoteJobs.Skill
   alias RemoteJobs.Profile
   alias RemoteJobs.Repo
 
@@ -21,6 +21,16 @@ defmodule RemoteJobs.ProfileOperator do
     |> get_by_id()
     |> Profile.changeset(attrs)
     |> Repo.update()
+  end
+
+	def add_skill(profile_id, skill) do
+		profile = profile_id |> get_by_id()
+		%Skill{description: skill, profile: profile} |> Repo.insert()
+	end
+
+  def delete_skill(skill_id) do
+		skill = Repo.get(Skill, skill_id)
+    skill |> Repo.delete!()
   end
 
 end
