a142179e1b56096b73ee6448cae9ec56508ca20d
Carlo Gilmar <carlogilmar12@gmail.com>
2019-10-17 13:23:29 -0500

    #85 Adding aplication track model

---
+++ lib/remote_jobs/models/application_track.ex
@@ -0,0 +1,22 @@
+defmodule RemoteJobs.ApplicationTrack do
+  @moduledoc """
+    Candidate track model definition
+  """
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias RemoteJobs.Application
+
+  @primary_key {:id, :id, autogenerate: true}
+  schema "application_tracks" do
+    field :description, :string
+    timestamps()
+    belongs_to :application, Application
+  end
+
+  @doc false
+  def changeset(job, attrs) do
+    job
+    |> cast(attrs, [:description])
+    |> validate_required([])
+  end
+end
