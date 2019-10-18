05d6ffaa79790b3a651aa9eb557c96f8dc316dcd
Carlo Gilmar <carlogilmar12@gmail.com>
2019-10-18 17:56:27 -0500

    #85 Adding functions for get applications

--- lib/remote_jobs/operator/job_application_operator.ex
+++ lib/remote_jobs/operator/job_application_operator.ex
@@ -1,5 +1,6 @@
 defmodule RemoteJobs.JobApplicationOperator do
   @moduledoc false
+  import Ecto.Query, only: [from: 2]
   alias RemoteJobs.JobApplication
   alias RemoteJobs.JobOperator
   alias RemoteJobs.ProfileOperator
@@ -13,6 +14,18 @@ defmodule RemoteJobs.JobApplicationOperator do
       profile: profile
     }
     |> Repo.insert()
+  end
+
+  def get(application_id), do: Repo.get(JobApplication, application_id)
+
+  def get_applications_by_profile(profile_id) do
+    query = from(j in JobApplication,  where: j.profile_id == ^profile_id)
+    Repo.all(query) |> Repo.preload([:application_track])
+  end
+
+  def get_applications_by_job(job_id) do
+    query = from(j in JobApplication,  where: j.job_id == ^job_id)
+    Repo.all(query) |> Repo.preload([:application_track])
   end
 
 end
