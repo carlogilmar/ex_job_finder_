3cb484845d381c45a4436a00f2ecac0a0ce8dfad
Carlo Gilmar <carlogilmar12@gmail.com>
2019-10-19 23:54:44 -0500

    #85 Set calendar ui

--- assets/js/controllers/application_controller.js
+++ assets/js/controllers/application_controller.js
@@ -1,9 +1,28 @@
 import Vue from 'vue'
 import socket from "./../socket"
+import VCalendar from 'v-calendar';
+import Notifications from 'vue-notification'
+Vue.use(Notifications)
+Vue.use(VCalendar)
 
 export const app = new Vue({
 	el:"#app",
   data: {
+    attributes: [
+      {
+        key: 'today',
+        dates: new Date(),
+          highlight: {
+            backgroundColor: '#000000',
+          },
+          contentStyle: {
+            color: '#00000',
+          },
+          popover: {
+            label: 'You just hovered over today\'s date!',
+          }
+      }
+    ],
 		loader: true,
 		application: []
   },
